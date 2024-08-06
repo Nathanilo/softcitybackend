a16c8d035ef3a7479a2f822cb40fd1ca3d229e57
nathanilo <nathanielodion@gmail.com>
2024-08-06 19:21:25 +0100

    Add product card component

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { defineProps } from "vue";
+import { RouterLink } from "vue-router";
+import { ref } from "vue";
+
+defineProps({
+  product: Object,
+});
+</script>
+
+<template>
+  <div class="p-4">
+    <div class="mb-6">
+      <h3 class="text-xl font-bold">{{ product.name }}</h3>
+    </div>
+
+    <div class="mb-5">
+      <img
+        :src="product.imageURL"
+        :alt="product.name"
+        class="w-full h-48 object-contain rounded-lg"
+      />
+    </div>
+
+    <h3 class="text-black mb-2">{{ product.description }}</h3>
+
+    <div class="border border-gray-100 mb-5"></div>
+
+    <div class="flex flex-col lg:flex-row justify-between mb-4">
+      <div class="text-[#e81101] mb-3">
+        <p class="text-2xl font-semibold">₦{{ product.price }}</p>
+      </div>
+      <router-link
+        :to="'/products/' + product.id"
+        class="h-[36px] bg-gray-700 hover:bg-[#e81101] text-white px-4 py-2 rounded-lg text-center text-sm"
+      >
+        view More
+      </router-link>
+    </div>
+  </div>
+</template>
--- src/components/ProductListings.vue
+++ src/components/ProductListings.vue
@@ -1,7 +1,9 @@
 <script setup>
 import productData from "@/products.json";
+import productCard from "@/components/ProductCard.vue";
 import { ref, defineProps } from "vue";
 import { RouterLink } from "vue-router";
+import ProductCard from "@/components/ProductCard.vue";
 
 defineProps({
   limit: Number,
@@ -22,41 +24,12 @@ console.log(products.value);
         Browse Products
       </h2>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <div
+        <ProductCard
           v-for="product in products"
           :key="product.id"
+          :product="product"
           class="bg-white rounded-xl shadow-md relative"
-        >
-          <div class="p-4">
-            <div class="mb-6">
-              <h3 class="text-xl font-bold">{{ product.name }}</h3>
-            </div>
-
-            <div class="mb-5">
-              <img
-                :src="product.imageURL"
-                :alt="product.name"
-                class="w-full h-48 object-contain rounded-lg"
-              />
-            </div>
-
-            <h3 class="text-black mb-2">{{ product.description }}</h3>
-
-            <div class="border border-gray-100 mb-5"></div>
-
-            <div class="flex flex-col lg:flex-row justify-between mb-4">
-              <div class="text-orange-700 mb-3">
-                {{ product.price }}
-              </div>
-              <router-link
-                :to="'/products/' + product.id"
-                class="h-[36px] bg-gray-700 hover:bg-[#e81101] text-white px-4 py-2 rounded-lg text-center text-sm"
-              >
-                view More
-              </router-link>
-            </div>
-          </div>
-        </div>
+        />
       </div>
     </div>
   </section>
