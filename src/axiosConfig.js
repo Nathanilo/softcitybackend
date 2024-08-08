c8f58d983899d16c08e8c5ff552365b771c18502
nathanilo <nathanielodion@gmail.com>
2024-08-08 12:23:01 +0100

    Bug fix

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Create an Axios instance for authentication API with default settings
 const authInstance = axios.create({
   baseURL: process.env.VUE_APP_API_BASE_URL || "http://localhost:5000",
-  timeout: 5000,
+  timeout: 60000,
   headers: {
     "Content-Type": "application/json",
   },
